ed4c79f86c5863e16fe725591675d2f4c3214218
Liana <lianavagyan.90@gmail.com>
2021-12-21 05:00:04 +0400

    feat: function isPositive is completed and added

---
+++ isPositive.js
@@ -0,0 +1,19 @@
+/* Complete the isPositive function below. It has one integer parameter a . If the value of a is positive, it must
+return the string YES. Otherwise, it must throw an Error according to the following rules:
+. If a is 0, throw an Error with message = Zero Error.
+. If a is negative, throw an Error with message = Negative Error. */
+
+function isPositive(a) {
+    try {
+        if (a > 0) {
+            return "YES";
+        } else if (a === 0) {
+            throw new Error("Zero Error");
+        } else {
+            throw new Error("Negative Error");
+        }
+    } catch(err) {
+        console.log(err.message);
+    }
+}
+console.log(isPositive(-8));
